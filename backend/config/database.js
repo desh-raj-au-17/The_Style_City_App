2098752dc57975db3ae75b453cd77f52f98bed92
desh-raj-au-17 <fullstackdev318@gmail.com>
2021-12-13 20:56:47 +0530

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,19 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((con) => {
+      console.log(
+        `MongoDB Database connected with HOST: ${con.connection.host}`
+      );
+    });
+};
+
+module.exports = connectDatabase;
---
+++ frontend/src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+const Footer = () => {
+  return (
+    <Fragment>
+      <footer className="py-1">
+        <p className="text-center mt-1">
+          Shopping Cart - 2021-2022, All Rights Reserved
+          <br />
+          Powerd by Attain-U & Design and Developed by Desh Raj and Vaishnavi
+          Gupta
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
+
+export default Footer;
